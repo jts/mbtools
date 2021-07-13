f0d43eb70ff7d035df12c97ff3333da075e36ab0
Jared Simpson <jared.simpson@gmail.com>
2021-07-13 16:56:29 -0400

    first pass at reading modification tags

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,83 @@
+//---------------------------------------------------------
+// Copyright 2021 Ontario Institute for Cancer Research
+// Written by Jared Simpson (jared.simpson@oicr.on.ca)
+//---------------------------------------------------------
 extern crate clap;
 use clap::{Arg, App, SubCommand, value_t};
+use rust_htslib::{bam, bam::Read, bam::record::Aux};
+
+// struct storing the modifications 
+pub struct ReadModifications
+{
+    canonical_base: char,
+    modified_base: char,
+    modification_indices: Vec<usize>,
+    modification_probabilities: Vec<f64>
+}
+
+impl ReadModifications
+{
+    pub fn from_bam_record(record: &bam::Record) -> Self {
+        println!("Parsing bam record");
+        let mut rm = ReadModifications {
+            canonical_base: 'x',
+            modified_base: 'x',
+            modification_indices: vec![],
+            modification_probabilities: vec![]
+        };
+
+        // TODO: remove when we switch bam_seq to match original strand of read
+        assert_eq!(record.is_reverse(), false);
+
+        //
+        let bam_seq = record.seq().as_bytes();
+        let mut modified_seq = bam_seq.clone();
+        let qname = std::str::from_utf8(record.qname()).unwrap();
+
+        // parse probabilities
+        if let Ok(Aux::ArrayU8(array)) = record.aux(b"Ml") {
+            for encoded_probability in array.iter() {
+                rm.modification_probabilities.push(encoded_probability as f64 / 255.0);
+            }
+        }
+
+        if let Ok(Aux::String(mm_str)) = record.aux(b"Mm") {
+
+            let mm_str_b = mm_str.as_bytes();
+            rm.canonical_base = mm_str_b[0] as char;
+            rm.modified_base = mm_str_b[2] as char;
+
+
+            // calculate the index in the read of each canonical base
+            let mut canonical_indices = Vec::<usize>::new();
+            for (index, base) in bam_seq.iter().enumerate() {
+                if *base as char == rm.canonical_base {
+                    canonical_indices.push(index)
+                }
+            }
+
+            // parse the modification string and transform it into indices
+            // into the read sequence
+            let mut canonical_count : usize = 0;
+            if mm_str.len() > 4 {
+                for token in mm_str[4..].split(",") {
+                    canonical_count += token.parse::<usize>().unwrap();
+                    rm.modification_indices.push(canonical_indices[canonical_count]);
+                    println!("token {} count: {}", token, canonical_count);
+                    canonical_count += 1;
+                }
+            }
+
+            // construct modified sequence (debug)
+            for index in &rm.modification_indices {
+                modified_seq[*index] = rm.modified_base as u8;
+            }
+        }
+
+        println!("Parsed {} -> {} modifications for {}\n{}", rm.canonical_base, rm.modified_base, qname, std::str::from_utf8(&modified_seq).unwrap());
+        rm
+    }
+}
 
 fn main() {
     let matches = App::new("mbtools")
@@ -29,5 +107,14 @@ fn main() {
 }
 
 fn calculate_modification_frequency(threshold: f64, input_bam: &str) {
-    println!("calculating modification frequency with t:{} on file {}", threshold, input_bam)
+    println!("calculating modification frequency with t:{} on file {}", threshold, input_bam);
+
+    let mut bam = bam::Reader::from_path(input_bam).unwrap();
+    //let header = bam::Header::from_template(bam.header());
+
+    //
+    for r in bam.records() {
+        let record = r.unwrap();
+        let modifications = ReadModifications::from_bam_record(&record);
+    }
 }
